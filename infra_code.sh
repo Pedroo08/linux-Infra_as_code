1c29ab31890c738b9c5e4e35bd56abbb4f45a997
Pedroo08 <pedroarcanjo8@gmail.com>
2022-10-12 18:11:36 +0000

    Ajuste na crianção de senha

--- infra_code.sh
+++ infra_code.sh
@@ -14,50 +14,25 @@ groupadd GRP_ADM
 groupadd GRP_VEN
 groupadd GRP_SEC
 
-echo"Criando usuários..."
+echo "Criando usuários..."
+  
 
-useradd carlos -m -s /bin/bash -c "Carlos Rocha" -G GRP_ADM
-echo"Criando senha de Carlos"
-passwd carlos 
-
-useradd maria -m -s /bin/bash -c "Maria da Silva" -G GRP_ADM
-echo"Criando senha de maria"
-passwd maria
-
-useradd joao -m -s /bin/bash -c "João Santos" -G GRP_ADM
-echo"Criando senha de joao"
-passwd  joao
+useradd carlos -m -s /bin/bash -p $(openssl passwd senha123)  -c "Carlos Rocha" -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwd senha123)  -c  "Maria da Silva" -G GRP_ADM
+useradd joao -m -s /bin/bash -p $(openssl passwd senha123)  -c "João Santos" -G GRP_ADM
 
 
-
-useradd debora -m -s /bin/bash -c "Debora da Costa" -G GRP_VEN
-echo"Criando senha de debora"
-passwd debora
-
-useradd sebastiana -m -s /bin/bash -c "Sebastiana da Invenção" -G GRP_VEN
-echo"Criando senha de sebastiana"
-passwd sebastiana
-
-useradd roberto -m -s /bin/bash -c "Roberto da Silva" -G GRP_VEN
-echo"Criando senha de roberto"
-passwd roberto
+useradd debora -m -s /bin/bash -p $(openssl passwd senha123) -c "Debora da Costa" -G GRP_VEN
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd senha123) -c "Sebastiana da Invenção" -G GRP_VEN
+useradd roberto -m -s /bin/bash -p $(openssl passwd senha123) -c "Roberto da Silva" -G GRP_VEN
 
 
-
-useradd josefina -m -s /bin/bash -c "Josefina Mattos " -G GRP_SEC
-echo"Criando senha de josefina"
-passwd josefina
-
-useradd amanda -m -s /bin/bash -c "Amanda Coelho" -G GRP_SEC
-echo"Criando senha de Amanda"
-passwd amanda
-
-useradd rogerio -m -s /bin/bash -c "Rogério Pato" -G GRP_SEC
-echo"Criando senha de rogerio"
-passwd rogerio
+useradd josefina -m -s /bin/bash -p $(openssl passwd senha123) -c "Josefina Mattos " -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $(openssl passwd senha123) -c "Amanda Coelho" -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $(openssl passwd senha123) -c "Rogério Pato" -G GRP_SEC
 
 
-echo "Especificando permissões dos diretórios.."
+echo "Especificando donos e  permissões dos diretórios.."
 chown root:GRP_ADM /adm
 chown root:GRP_VEN /ven
 chown root:GRP_SEC /sec
@@ -67,7 +42,7 @@ chmod 770 /ven
 chmod 770 /sec
 chmod 777 /publico 
 
-echo"...Finalizado..."
+echo "...Finalizado..."
 
 
 
